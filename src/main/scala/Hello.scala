b92dd1be9a1ab66bf468624ad6f5f78c6aa66609
Nikita Kolybelkin <kolybelkinnm@pik.ru>
2020-10-30 22:41:21 +0300

    dsafikjag

---
+++ src/main/scala/Hello.scala
@@ -0,0 +1,10 @@
+//import scala.annotation.tailrec
+
+
+
+
+object Hello extends App {
+  def MySort[T](arr: List[T], order: (T, T) => Boolean): List[T] = {
+  }
+  println(MySort(List(10, 20), (a: Int, b: Int) => a < b))
+}
